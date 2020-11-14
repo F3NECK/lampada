f0598d4fb9e95abd8df2c8d58c1b4f542a61627e
Felipe Magno <felipehmagno@gmail.com>
2020-11-14 17:06:29 -0300

    .

---
+++ main.js
@@ -0,0 +1,25 @@
+
+/*
+var lampada = document.getElementById("img").src;
+var ligado = "ligado.png"
+var desligado = "desligado.png"
+
+function toogle() {
+    if(lampada = desligado) {
+        document.getElementById("img").src = ligado;
+
+    } else (lampada= ligado){
+            document.getElementById("img").src = desligado;
+        }
+        
+    
+}*/
+
+function toogle(){
+    lampada = document.getElementById("img").scroll;
+    if (lampada = "deligado.png") {
+        document.getElementById("img").src = "ligado.png"
+    } else {
+        document.getElementById("img").src = "desligado.png"
+    }
+}
